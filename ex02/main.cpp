c56b71ff218a9319fa2da6606f2e799c6c3694a2
Junya Kosaka <kosakajunya@gmail.com>
2022-06-12 17:44:30 +0900

    ...

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,9 +4,12 @@ int main(void) {
 	std::string s = "HI THIS IS BRAIN";
 	std::string* stringPTR = &s;
 	std::string& stringREF = s;
+
 	std::cout << &s << std::endl;
 	std::cout << stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
+
+	std::cout << s << std::endl;
 	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
 }
