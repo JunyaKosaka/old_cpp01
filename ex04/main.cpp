b6e74737d9062f60c1549b35429974137fe6e0ce
Junya Kosaka <kosakajunya@gmail.com>
2022-06-13 21:06:49 +0900

    fix: ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,27 +3,26 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-		std::cout << "INVALID INPUT!" << std::endl;
-        std::cout << "INPUT LIKE THIS ./a.out <filename> <s1> <s2>" << std::endl;
-        return 1;
-    }
+  if (argc != 4) {
+    std::cout << "[USAGE] ./a.out <filename> <s1> <s2>" << std::endl;
+    return 1;
+  }
 	
-    const std::string input_file(argv[1]);
+  const std::string input_file(argv[1]);
 	std::ifstream ifs(input_file);
-    if (ifs.fail()) {
-        std::cout << "Failed to open " << input_file << std::endl;
-        return 1;
-    }
+  if (ifs.fail()) {
+    std::cout << "Failed to open " << input_file << std::endl;
+    return 1;
+  }
 
-    const std::string output_file(input_file + ".replace");
+  const std::string output_file(input_file + ".replace");
 	std::ofstream ofs(output_file);
-    if (ofs.fail()) {
+  if (ofs.fail()) {
 		std::cout << "Failed to open " << output_file << std::endl;
-        return 1;
-    }
+    return 1;
+  }
 
-    const std::string s1(argv[2]), s2(argv[3]);
+  const std::string s1(argv[2]), s2(argv[3]);
 	std::string file_str(""), line("");
 	while (1) {
 		std::getline(ifs, line);
@@ -34,18 +33,17 @@ int main(int argc, char *argv[]) {
 		file_str += '\n';
 	}
 
-    std::string::size_type pos = file_str.find(s1);
-    while (pos != std::string::npos) {
+  std::string::size_type pos = file_str.find(s1);
+  while (pos != std::string::npos) {
 		for (int i = 0; i < int(s1.length()); i++) {
-			*(pos+i) = s2[i];
+			file_str[pos+i] = s2[i];
 		}
-        // file_str.replace(pos, s1.length(), s2); // .replaceは使えない
-        pos = file_str.find(s1, pos + s2.length());
-    }
+    pos = file_str.find(s1, pos + s2.length());
+  }
 	ofs << file_str << std::endl;
 
 	ifs.close();
 	ofs.close();
 
-    return 0;
+  return 0;
 }
