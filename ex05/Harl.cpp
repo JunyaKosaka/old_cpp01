18e7177e15f5d6d5e17ce55393a5f2226bcfb2a9
Junya Kosaka <kosakajunya@gmail.com>
2022-06-14 14:14:50 +0900

    style: cpplint

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -12,19 +12,19 @@ void Harl::debug(void) {
 
 void Harl::info(void) {
   std::cout << "INFO: I cannot believe adding extra bacon costs more money. "
-    "You didn’t putenough bacon in my burger! If you did, I wouldn’t be asking for more!"
-    << std::endl;
+    "You didn’t putenough bacon in my burger! If you did, I wouldn’t "
+    "be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) {
-  std::cout << "WARNING: I think I deserve to have some extra bacon for free. I’ve been coming for "
-    "years whereas you started working here since last month."
-    << std::endl;
+  std::cout << "WARNING: I think I deserve to have some extra bacon for free. "
+    "I’ve been coming for years whereas you started working here "
+    "since last month." << std::endl;
 }
 
 void Harl::error(void) {
-  std::cout << "ERROR: This is unacceptable! I want to speak to the manager now."
-    << std::endl;
+  std::cout << "ERROR: This is unacceptable! "
+    "I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level) {
